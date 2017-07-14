e3faef89220d85cb9bd0c01a14ee3c3a677dfa98
lebashir <bashir.abihabib@gmail.com>
2017-07-14 18:16:46 -0500

    completed user-story-2-initialize

--- lib/kele.rb
+++ lib/kele.rb
@@ -3,9 +3,9 @@ require 'httparty'
 class Kele
   include HTTParty
 
-  def initialze(email, password)
+  def initialize(email, password)
     @base_url = "https://www.bloc.io/api/v1"
-    response = self.class.post("https://www.bloc.io/api/v1/sessions", body: {"email": email, "password": password})
+    response = self.class.post('https://www.bloc.io/api/v1/sessions', body: { email: email, password: password} )
     raise "invalid email/password" if response.code != 200
     @auth_token = response["auth_token"]
   end
