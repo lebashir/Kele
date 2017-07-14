1c900516b968cf93ce7d9b2486e5d191b7a98207
lebashir <bashir.abihabib@gmail.com>
2017-07-14 18:26:56 -0500

    completed user-story-3-retrieve-users

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,12 +1,19 @@
 require 'httparty'
+require 'json'
 
 class Kele
   include HTTParty
+  include JSON
 
   def initialize(email, password)
     @base_url = "https://www.bloc.io/api/v1"
     response = self.class.post('https://www.bloc.io/api/v1/sessions', body: { email: email, password: password} )
     raise "invalid email/password" if response.code != 200
     @auth_token = response["auth_token"]
+  end
+
+  def get_me
+    response = self.class.get('https://www.bloc.io/api/v1/users/me', headers: { "authorization" => @auth_token })
+    JSON.parse(response.body)
   end
 end
