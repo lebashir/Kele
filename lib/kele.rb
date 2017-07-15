ff2be9ab79b8cad1f6bf22b5828745ddfe3ad101
lebashir <bashir.abihabib@gmail.com>
2017-07-14 19:30:28 -0500

    fixed some formatting im pretty sure messaging is working too

--- lib/kele.rb
+++ lib/kele.rb
@@ -9,19 +9,32 @@ class Kele
 
   def initialize(email, password)
     @base_url = "https://www.bloc.io/api/v1"
-    response = self.class.post('https://www.bloc.io/api/v1/sessions', body: { email: email, password: password} )
+    response = self.class.post('/sessions', body: { email: email, password: password} )
     raise "invalid email/password" if response.code != 200
     @auth_token = response["auth_token"]
   end
 
   def get_me
-    response = self.class.get('https://www.bloc.io/api/v1/users/me', headers: { "authorization" => @auth_token })
+    response = self.class.get('/users/me', headers: { "authorization" => @auth_token })
     JSON.parse(response.body)
   end
 
   def get_mentor_availability(mentor_id)
-    response = self.class.get('https://www.bloc.io/api/v1/mentors/'+(mentor_id.to_s)+'/student_availability', headers: { "authorization" => @auth_token})
+    response = self.class.get('/mentors/#{mentor_id}/student_availability', headers: { "authorization" => @auth_token})
     JSON.parse(response.body)
+  end
+
+  def get_messages(page = nil)
+    if page == nil
+      response = self.class.get('/message_threads/', headers: { "authorization" => @auth_token })
+    else
+      response = self.class.get('/message_threads>page=#{page_num}', headers: { "authorization" => @auth_token })
+    end
+    JSON.parse(response.body)
+  end
+
+  def create_message(sender, recipient_id, subject, stripped-text)
+    respose = self.class.get('/messages', body: { sender: sender, recipient_id: recipient_id, subject: subject, stripped-text: stripped-text}, headers: {"authorization" => @auth_token })
   end
 
 end
--- lib/roadmap.rb
+++ lib/roadmap.rb
@@ -1,11 +1,12 @@
 module Roadmap
-  def get_roadmap(roadmap_id)
-    response = self.class.get('https://www.bloc.io/api/v1/roadmaps/'+(roadmap_id.to_s), headers: { "authorization" => @auth_token })
+
+  def get_roadmap(id)
+    response = self.class.get('https://www.bloc.io/api/v1/roadmaps/#{id}', headers: { "authorization" => @auth_token })
     JSON.parse(response.body)
   end
 
-  def get_checkpoint(checkpoint_id)
-    response = self.class.get('https://www.bloc.io/api/v1/checkpoints/'+(checkpoint_id.to_s), headers: { "authorization" => @auth_token })
+  def get_checkpoint(id)
+    response = self.class.get('https://www.bloc.io/api/v1/checkpoints/#{id}', headers: { "authorization" => @auth_token })
     JSON.parse(response.body)
   end
 end
